177b2c5a073656924b645c27fce1325541f182ed
Your Name <you@example.com>
2022-01-31 09:10:37 +0300

    added in a news and articles class to set the order i want those objects to to stand in

---
+++ app/models.py
@@ -0,0 +1,22 @@
+class News:
+    '''
+    define News Objects
+    '''
+    def __init__(self,id,name,url,description,category, country):
+        self.id =id
+        self.name= name
+        self.url = url
+        self.description = description
+        self.category = category
+        self.country = country
+    #call out 547    
+class Articles:
+    get_articles = []
+    def __init__(self,title,author,url,description, urlToImage, publishedAt, content):
+        self.title = title
+        self.author = author
+        self.url = url
+        self.description= description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
