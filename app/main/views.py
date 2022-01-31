86c0257a911af4dad39f20310fc902af598f9d81
Your Name <you@example.com>
2022-01-31 08:08:20 +0300

    set up my errors page, called my .html pages as well as set up my blueprint in my main __init__.py

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from ..request import get_news,get_articles
+from flask import render_template,request,redirect,url_for
+from . import main
+#Views
+@main.route('/')
+def index():
+    current_news = get_news()
+    print(current_news)
+    title = "Welcome to my news hub"
+    return render_template('index.html', title= title, current_news = current_news )
+@main.route("/news/<news_id>")
+def articles(news_id):
+    articles = get_articles(news_id)
+    print(articles)
+    title = f'{news_id}'
+    return render_template("articles.html", articles = articles, title = title)
